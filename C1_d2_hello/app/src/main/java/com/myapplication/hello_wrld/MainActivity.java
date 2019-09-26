a04425cb69ec06cc302151c23b155d62d9d380a6
mpolyakov <polyakov4@yandex.ru>
2019-09-26 23:42:03 +0300

    + картинка, image button, onClick, PlainText, TextView

--- C1_d2_hello/app/src/main/java/com/myapplication/hello_wrld/MainActivity.java
+++ C1_d2_hello/app/src/main/java/com/myapplication/hello_wrld/MainActivity.java
@@ -3,12 +3,32 @@ package com.myapplication.hello_wrld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private EditText mSomethingEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHelloTextView = findViewById(R.id.textView);
+        mSomethingEditText = findViewById(R.id.editText);
+    }
+
+    public void onClick(View view) {
+
+        if (mSomethingEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Everybody!");
+        } else {
+            mHelloTextView.setText("Hello " + mSomethingEditText.getText() + "!" );
+        }
+
     }
 }
