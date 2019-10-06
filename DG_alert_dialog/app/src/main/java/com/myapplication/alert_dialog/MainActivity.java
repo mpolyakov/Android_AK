a94e4bd098864477991801ebddc5c600c47166c3
mpolyakov <polyakov4@yandex.ru>
2019-10-06 23:41:25 +0300

    some corrections in code

--- DG_alert_dialog/app/src/main/java/com/myapplication/alert_dialog/MainActivity.java
+++ DG_alert_dialog/app/src/main/java/com/myapplication/alert_dialog/MainActivity.java
@@ -1,14 +1,55 @@
 package com.myapplication.alert_dialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private RatingBar rating;
+    private TextView text_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        someDifferentMakings();
+
+
+
+    }
+
+    void someDifferentMakings(){
+        rating = findViewById(R.id.ratingBar);
+        text_show = findViewById(R.id.textView);
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                text_show.setText("Значение: " + String.valueOf(Math.round(v)));
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+        a_builder.setMessage("Вы хотите закрыть приложение?").setCancelable(false).setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        }).setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Закрытие приложения");
+        alert.show();
     }
 }
