e3e3634de1ab5d8bdf9787471e87ae26255134ae
mpolyakov <polyakov4@yandex.ru>
2019-11-06 00:10:50 +0300

    WebSettings. Просмотр веб-страниц в приложении. Реализация возврата через кнопку "назад".

---
+++ DG_web-brows/app/src/main/java/com/myapplication/webbrows/MainActivity.java
@@ -0,0 +1,34 @@
+package com.myapplication.webbrows;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        webView.loadUrl("https://google.com");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
