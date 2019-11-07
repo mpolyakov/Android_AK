f56be48525c1b0186c3f46a9f76e6b3d9048ed59
mpolyakov <polyakov4@yandex.ru>
2019-11-07 23:14:00 +0300

    some "my browser" exercices

---
+++ C1_d18_MyBrowser/app/src/main/java/com/myapplication/mybrowser/MainActivity.java
@@ -0,0 +1,22 @@
+package com.myapplication.mybrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://google.ru");
+
+    }
+}
