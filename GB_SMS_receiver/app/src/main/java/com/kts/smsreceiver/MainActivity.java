fd4bbfde0fdc82f9fbe9d047fac9974d1e71770f
mpolyakov <polyakov4@yandex.ru>
2019-12-22 23:35:58 +0300

    Приём и отсылка СМС-сообщений

---
+++ GB_SMS_receiver/app/src/main/java/com/kts/smsreceiver/MainActivity.java
@@ -0,0 +1,42 @@
+package com.kts.smsreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String number;
+    String message;
+    TextInputEditText textInputEditText;
+    EditText numberEditText;
+    public static TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void send(View view) {
+        textInputEditText = findViewById(R.id.txtInputEditTxt);
+        message = textInputEditText.getText().toString();
+        numberEditText = findViewById(R.id.editText);
+        number = numberEditText.getText().toString();
+
+        String toNumberSms = "smsto:" + number;
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse(toNumberSms));
+        smsIntent.putExtra("sms_body", message);
+        startActivity(smsIntent);
+
+    }
+}
