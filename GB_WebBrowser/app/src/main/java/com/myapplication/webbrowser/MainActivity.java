c8deca31d447e2f28156a7ebae0761441ef0383b
mpolyakov <polyakov4@yandex.ru>
2019-12-25 21:16:28 +0300

    https -> http

--- GB_WebBrowser/app/src/main/java/com/myapplication/webbrowser/MainActivity.java
+++ GB_WebBrowser/app/src/main/java/com/myapplication/webbrowser/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
@@ -49,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                        HttpsURLConnection urlConnection;
-                        urlConnection = (HttpsURLConnection) uri.openConnection();
+                        HttpURLConnection urlConnection;
+                        urlConnection = (HttpURLConnection) uri.openConnection();
                         urlConnection.setRequestMethod("GET");
 
                         urlConnection.setReadTimeout(10000);
