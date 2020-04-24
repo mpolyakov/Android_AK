3addb2e0b5f1b6d356162ab21d0da0c04ba46c20
mpolyakov <polyakov4@yandex.ru>
2020-04-24 22:35:51 +0300

    example CardView , RecyclerView

---
+++ Udemy_RecyclerView/app/src/main/java/com/kt/std/recyclerview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.kt.std.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<RecyclerViewItem> recyclerViewItems = new ArrayList<>();
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_sentiment_satisfied_black_24dp, "Happy", "Good!"));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_sentiment_dissatisfied_black_24dp, "Sad", "Sad"));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_sentiment_neutral_black_24dp, "Neutral", "Life is life!"));
+    }
+}
---
+++ Udemy_RecyclerView/app/src/main/java/com/kt/std/recyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,17 @@
+package com.kt.std.recyclerview;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter {
+public static class RecyclerViewViewHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerViewViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
+
+
+}
