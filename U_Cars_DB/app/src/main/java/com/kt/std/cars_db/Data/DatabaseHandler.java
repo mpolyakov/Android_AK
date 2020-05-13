145d2dc27268865bb8b7fec06b09bdfd20875850
mpolyakov <polyakov4@yandex.ru>
2020-05-13 11:24:27 +0300

    приложение - база данных автомобилей. добавлен класс - модель данных. класс - создание и апгрейд БД

---
+++ U_Cars_DB/app/src/main/java/com/kt/std/cars_db/Data/DatabaseHandler.java
@@ -0,0 +1,34 @@
+package com.kt.std.cars_db.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.kt.std.cars_db.Utils.Util;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_CARS_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY,"
+                + Util.KEY_NAME + " TEXT,"
+                + Util.KEY_PRICE + " TEXT" + ")";
+
+        sqLiteDatabase.execSQL(CREATE_CARS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
+    }
+
+}
---
+++ U_Cars_DB/app/src/main/java/com/kt/std/cars_db/Utils/Util.java
@@ -0,0 +1,13 @@
+package com.kt.std.cars_db.Utils;
+
+public class Util {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "carsDB";
+    public static final String TABLE_NAME = "cars";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_PRICE = "name";
+    public static final String KEY_NAME = "price";
+
+
+}
