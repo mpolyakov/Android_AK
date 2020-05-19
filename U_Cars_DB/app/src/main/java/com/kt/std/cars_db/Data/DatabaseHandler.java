8c1c524e921038a8cbe20590605ce4213460f95c
mpolyakov <polyakov4@yandex.ru>
2020-05-19 11:43:59 +0300

    код для добавления и удаления записей в БД

--- U_Cars_DB/app/src/main/java/com/kt/std/cars_db/Data/DatabaseHandler.java
+++ U_Cars_DB/app/src/main/java/com/kt/std/cars_db/Data/DatabaseHandler.java
@@ -1,11 +1,14 @@
 package com.kt.std.cars_db.Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.kt.std.cars_db.Model.Car;
 import com.kt.std.cars_db.Utils.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -28,7 +31,50 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
         onCreate(sqLiteDatabase);
+    }
+
+    public void addCar(Car car) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+
+        db.insert(Util.TABLE_NAME, null, contentValues);
+        db.close();
 
     }
 
+    public Car getCar(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[] {Util.KEY_ID, Util.KEY_NAME, Util.KEY_PRICE}, Util.KEY_ID + "=?"
+        + new String[] {String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        Car car = new Car(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
+        return car;
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
