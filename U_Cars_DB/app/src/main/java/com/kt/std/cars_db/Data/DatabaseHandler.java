84febc4a76293608e57dfcc4b65cba954e54fbe4
mpolyakov <polyakov4@yandex.ru>
2020-05-20 13:13:19 +0300

    полностью реализован функционал CRUD для созданной БД

--- U_Cars_DB/app/src/main/java/com/kt/std/cars_db/Data/DatabaseHandler.java
+++ U_Cars_DB/app/src/main/java/com/kt/std/cars_db/Data/DatabaseHandler.java
@@ -11,6 +11,9 @@ import androidx.annotation.Nullable;
 import com.kt.std.cars_db.Model.Car;
 import com.kt.std.cars_db.Utils.Util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseHandler extends SQLiteOpenHelper {
     public DatabaseHandler(Context context) {
         super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
@@ -48,17 +51,57 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public Car getCar(int id){
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query(Util.TABLE_NAME, new String[] {Util.KEY_ID, Util.KEY_NAME, Util.KEY_PRICE}, Util.KEY_ID + "=?"
-        + new String[] {String.valueOf(id)}, null, null, null, null);
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[] {Util.KEY_ID, Util.KEY_NAME, Util.KEY_PRICE}, Util.KEY_ID + "=?",
+        new String[] {String.valueOf(id)}, null, null, null, null);
 
         if (cursor != null) {
             cursor.moveToFirst();
         }
         Car car = new Car(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2));
         return car;
+    }
 
+    public List<Car> getAllCars (){
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<Car> carsList = new ArrayList<>();
+        String selectAllCars = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectAllCars, null);
+        if (cursor.moveToFirst()){
+            do {
+                Car car = new Car();
+                car.setId(Integer.parseInt(cursor.getString(0)));
+                car.setName(cursor.getString(1));
+                car.setPrice(cursor.getString(2));
+                carsList.add(car);
+            } while (cursor.moveToNext());
+        }
+        return carsList;
+    }
+
+    public int updateCar (Car car){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+
+         return db.update(Util.TABLE_NAME, contentValues, Util.KEY_ID + "=?", new String[] {String.valueOf(car.getId())});
 
     }
+
+    public void deleteCar(Car car){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Util.TABLE_NAME, Util.KEY_ID + "=?", new String[] {String.valueOf(car.getId())});
+        db.close();
+    }
+
+    public int getCarsCount(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String countCar = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = db.rawQuery(countCar, null);
+        return cursor.getCount();
+    }
+
 }
 
 
--- U_Cars_DB/app/src/main/java/com/kt/std/cars_db/MainActivity.java
+++ U_Cars_DB/app/src/main/java/com/kt/std/cars_db/MainActivity.java
@@ -3,6 +3,12 @@ package com.kt.std.cars_db;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kt.std.cars_db.Data.DatabaseHandler;
+import com.kt.std.cars_db.Model.Car;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler databaseHandler = new DatabaseHandler(this);
+        Log.d("CarsCount", String.valueOf(databaseHandler.getCarsCount()));
+
+//        databaseHandler.addCar(new Car("Toyota", "30000 $"));
+//        databaseHandler.addCar(new Car("Opel", "25000 $"));
+//        databaseHandler.addCar(new Car("Mers", "50000 $"));
+//        databaseHandler.addCar(new Car("Kia", "40000 $"));
+//
+//        List<Car> carList = databaseHandler.getAllCars();
+//
+//        for (Car car : carList) {
+//            Log.d("CarInfo", "ID " + car.getId() + " , name " + car.getName() + " , price " + car.getPrice());
+//        }
+
+        Car car = databaseHandler.getCar(10);
+        Log.d("CarInfo", "ID " + car.getId() + " , name " + car.getName() + " , price " + car.getPrice());
+
+        car.setName("Tesla");
+        car.setPrice("63251$");
+        databaseHandler.updateCar(car);
+
+        databaseHandler.deleteCar(car);
+
+        List<Car> carList = databaseHandler.getAllCars();
+
+        for (Car c : carList) {
+            Log.d("CarInfo", "ID " + c.getId() + " , name " + c.getName() + " , price " + c.getPrice());
+        }
+
+
+
     }
 }
