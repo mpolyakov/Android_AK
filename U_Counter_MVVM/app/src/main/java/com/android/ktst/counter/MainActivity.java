fb7a7645fe3c17a6995ea90336805db6d7a0b1f9
mpolyakov <polyakov4@yandex.ru>
2020-06-13 16:50:32 +0300

    отработка архитектуры MVVM. Использование ViewModel для сохранения данных.

---
+++ U_Counter_MVVM/app/src/main/java/com/android/ktst/counter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.android.ktst.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    MainActivityViewModel model;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        textView = findViewById(R.id.textView);
+        textView.setText(String.valueOf(model.getCurrentValue()));
+    }
+
+    public void decreaseValue(View view) {
+        textView.setText(String.valueOf(model.getDecreasedValue()));
+    }
+
+    public void increaseValue(View view) {
+        textView.setText(String.valueOf(model.getIncreasedValue()));
+    }
+
+
+}
---
+++ U_Counter_MVVM/app/src/main/java/com/android/ktst/counter/MainActivityViewModel.java
@@ -0,0 +1,19 @@
+package com.android.ktst.counter;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    private int count = 1;
+
+    public int getDecreasedValue() {
+        return --count;
+    }
+
+    public int getIncreasedValue() {
+        return ++count;
+    }
+
+    public int getCurrentValue() {
+        return count;
+    }
+}
