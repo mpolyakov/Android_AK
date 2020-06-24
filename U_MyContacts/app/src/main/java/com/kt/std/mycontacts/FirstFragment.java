b2ab10e689f739c0f71ab0483714f970df05d212
mpolyakov <polyakov4@yandex.ru>
2020-06-24 13:43:08 +0300

    добавление Recycler View адаптера

--- U_MyContacts/app/src/main/java/com/kt/std/mycontacts/FirstFragment.java
+++ U_MyContacts/app/src/main/java/com/kt/std/mycontacts/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.kt.std.mycontacts;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import java.util.ArrayList;
 
 public class FirstFragment extends Fragment {
+    private MyContactsDatabase myContactsDatabase;
+    private ArrayList<Contact> contactArrayList;
+    private ContactAdapter contactAdapter;
 
     @Override
     public View onCreateView(
@@ -23,12 +32,89 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        myContactsDatabase = Room.databaseBuilder(getContext(), MyContactsDatabase.class, "ContactsDB").build();
+        loadContacts();
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+        contactAdapter = new ContactAdapter();
+        recyclerView.setAdapter(contactAdapter);
+
+    }
+
+    private void loadContacts() {
+        new FirstFragment.GetAllContactsAsyncTask().execute();
+    }
+
+    private void deleteContact(Contact contact){
+        new FirstFragment.DeleteContactAsyncTask().execute(contact);
+    }
+
+    private void addContact(Contact contact){
+        new FirstFragment.AddContactAsyncTask().execute(contact);
+    }
+
+    private class GetAllContactsAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            contactArrayList = (ArrayList<Contact>) myContactsDatabase.getContactDao().getAllContacts();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            contactAdapter.setContactArrayList(contactArrayList);
+        }
+    }
+    private class DeleteContactAsyncTask extends AsyncTask<Contact, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+            myContactsDatabase.getContactDao().deleteContact(contacts[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            loadContacts();
+        }
+    }
+
+    private class AddContactAsyncTask extends AsyncTask<Contact, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+            myContactsDatabase.getContactDao().insertContact(contacts[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            loadContacts();
+        }
+    }
+
+    private class UpdateContactAsyncTask extends AsyncTask<Contact, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Contact... contacts) {
+            myContactsDatabase.getContactDao().updateContact(contacts[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            loadContacts();
+        }
     }
 }
--- U_MyContacts/app/src/main/java/com/kt/std/mycontacts/MainActivity.java
+++ U_MyContacts/app/src/main/java/com/kt/std/mycontacts/MainActivity.java
@@ -1,25 +1,31 @@
 package com.kt.std.mycontacts;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private MyContactsDatabase myContactsDatabase;
-    private ArrayList<Contact> contactArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +34,58 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        myContactsDatabase = Room.databaseBuilder(getApplicationContext(), MyContactsDatabase.class, "ContactsDB").build();
-        loadContacts();
-        
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addAndEditContact(false, null, -1);
             }
         });
     }
 
-    private void loadContacts() {
-        new GetAllContactsAsyncTask().execute();
+    public void addAndEditContact(final boolean isUpdate, Contact contact, long position) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflater.inflate(R.layout.add_contact_edit, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setView(view);
+
+        TextView contactTitleTextView = view.findViewById(R.id.contactTitle);
+        final EditText firstNameEditText = view.findViewById(R.id.firstNameEditText);
+        final EditText lastNameEditText = view.findViewById(R.id.lastNameEditText);
+        final EditText emailEditText = view.findViewById(R.id.emailEditText);
+        final EditText phoneNumberEditText = view.findViewById(R.id.phoneNumberEditText);
+
+        contactTitleTextView.setText(!isUpdate ? "Add contact" : "Edit contact");
+        if (isUpdate && (contact != null)) {
+            firstNameEditText.setText(contact.getFirstName());
+            lastNameEditText.setText(contact.getLastName());
+            emailEditText.setText(contact.getEmail());
+            phoneNumberEditText.setText(contact.getPhoneNumber());
+
+            builder.setCancelable(false).setPositiveButton(isUpdate ? "Update" : "Save", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if (TextUtils.isEmpty(firstNameEditText.getText().toString())) {
+                        Toast.makeText(MainActivity.this, "Enter first name", Toast.LENGTH_SHORT).show();
+                    } else  if (TextUtils.isEmpty(lastNameEditText.getText().toString())) {
+                        Toast.makeText(MainActivity.this, "Enter first name", Toast.LENGTH_SHORT).show();
+                    } else  if (TextUtils.isEmpty(emailEditText.getText().toString())) {
+                        Toast.makeText(MainActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    } else  if (TextUtils.isEmpty(phoneNumberEditText.getText().toString())) {
+                        Toast.makeText(MainActivity.this, "Enter phone number", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (isUpdate && (contact != null)){
+
+                        }
+                    }
+                }
+            });
+
+        }
     }
 
-    private void deleteContact(Contact contact){
-        new DeleteContactAsyncTask().execute(contact);
-    }
-
-    private void addContact(Contact contact){
-        new AddContactAsyncTask().execute(contact);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,30 +109,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class GetAllContactsAsyncTask extends AsyncTask<Void, Void, Void>{
-        @Override
-        protected Void doInBackground(Void... voids) {
 
-            contactArrayList = (ArrayList<Contact>) myContactsDatabase.getContactDao().getAllContacts();
-
-            return null;
-        }
-    }
-    private class DeleteContactAsyncTask extends AsyncTask<Contact, Void, Void>{
-
-        @Override
-        protected Void doInBackground(Contact... contacts) {
-            myContactsDatabase.getContactDao().deleteContact(contacts[0]);
-            return null;
-        }
-    }
-
-    private class AddContactAsyncTask extends AsyncTask<Contact, Void, Void>{
-
-        @Override
-        protected Void doInBackground(Contact... contacts) {
-            myContactsDatabase.getContactDao().insertContact(contacts[0]);
-            return null;
-        }
-    }
 }
